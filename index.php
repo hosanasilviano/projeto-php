ee3ae209a7d377862f100630ea74837b300d251f
hosanasilviano <naninhafanfic@gmail.com>
2018-06-21 21:07:19 -0300

    inicio do projeto php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	$titulo = "Título da página";
+	$tituloconteudo ="<h1>Título do texto<h1>";
+	$conteudo = "<p>GIT - Versionamento</p>";
+?>
+
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<Meta charset= "utf-8" />
+</head>
+<body>
+<?php
+	echo $tituloconteudo;
+	echo $conteudo;
+?>
+</body>
+</html>
+
